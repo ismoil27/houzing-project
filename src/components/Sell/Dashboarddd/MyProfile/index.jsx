bf4c1802e3c0047c39931d8c420a23ba13c0a360
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2022-03-28 13:05:16 +0900

    done My Profile

--- src/components/Sell/Dashboarddd/MyProfile/index.jsx
+++ src/components/Sell/Dashboarddd/MyProfile/index.jsx
@@ -1,10 +1,66 @@
 import React from "react";
-import { Container } from "./style";
-
+import { Body, ChangePassword, Container, Items } from "./style";
+import image from "../../../assets/images/card2.jpeg";
 const MyProfile = () => {
   return (
     <Container>
-      <h1>Hello</h1>
+      <Container.Title>
+        <h1>My Profile</h1>
+        <span>Ready to jump back in?</span>
+      </Container.Title>
+      <Body>
+        <Container.Content>
+          <Container.Contact>
+            <Container.ContentTitle>Contact Information</Container.ContentTitle>
+            <Items>
+              <Container.Input placeholder="First Name" />
+              <Container.Input margin="true" placeholder="Last Name" />
+            </Items>
+            <Items>
+              <Container.Input placeholder="Email" />
+              <Container.Input margin="true" placeholder="Phone" />
+            </Items>
+            <Items>
+              <Container.Input placeholder="Mobile" />
+              <Container.Input margin="true" placeholder="Skype" />
+            </Items>
+            <Container.ContentTitle>Social Media</Container.ContentTitle>
+            <Items>
+              <Container.Input placeholder="Facebook Url" />
+              <Container.Input margin="true" placeholder="Twitter Url" />
+            </Items>
+            <Items>
+              <Container.Input placeholder="Linkedin Url" />
+              <Container.Input margin="true" placeholder="Instagram Url" />
+            </Items>
+            <Container.TextArea rows="10" placeholder="About me" />
+            <Container.Button>UPDATE PROFILE </Container.Button>
+          </Container.Contact>
+
+          <ChangePassword>
+            <Container.ContentTitle>Contact Information</Container.ContentTitle>
+
+            <Items>
+              <Container.Input placeholder="Old Password" />
+            </Items>
+            <Items>
+              <Container.Input placeholder="New Password" />
+              <Container.Input
+                margin="true"
+                placeholder="Confirm New Password"
+              />
+            </Items>
+            <Container.Button>CHANGE PASSWORD </Container.Button>
+          </ChangePassword>
+        </Container.Content>
+
+        <Container.Img>
+          <h2>Photo</h2>
+          <img src={image} alt="" />
+          <Container.Upload>UPLOAD PROFILE PHOTO</Container.Upload>
+          <span>*minimum 500px x 500px</span>
+        </Container.Img>
+      </Body>
     </Container>
   );
 };
--- src/components/Sell/Dashboarddd/MyProfile/style.js
+++ src/components/Sell/Dashboarddd/MyProfile/style.js
@@ -3,10 +3,174 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  /* background-color: rgb(246, 248, 249); */
   background: #e5e5e5;
   width: 100%;
   padding: 60px;
 `;
 
-export { Container };
+Container.Title = styled.div`
+  h1 {
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 38px;
+    color: var(--color-bg);
+  }
+  span {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    color: #696969;
+  }
+  margin-bottom: 60px;
+`;
+
+const Body = styled.div`
+  display: flex;
+`;
+
+Container.Content = styled.div`
+  flex: 3;
+`;
+
+Container.Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: var(--color-white);
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  padding: 30px;
+  margin-bottom: 30px;
+`;
+
+Container.ContentTitle = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: var(--color-bg);
+  margin-bottom: 45px;
+`;
+
+const Items = styled.div`
+  display: flex;
+  margin-bottom: 40px;
+  align-items: center;
+`;
+
+Container.Input = styled.input`
+  outline: none;
+  width: 100%;
+  border: none;
+  border-bottom: 2px solid #e6e9ec;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  color: #696969;
+  padding-bottom: 8px;
+  margin-left: ${({ margin }) => (margin === "true" ? "30px" : "")};
+  :focus {
+    border-color: #0061df;
+    ::placeholder {
+      color: #000;
+    }
+  }
+`;
+
+Container.TextArea = styled.textarea`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  color: #696969;
+  border: none;
+  outline: none;
+  margin-bottom: 30px;
+  border-bottom: 2px solid #e6e9ec;
+  :focus {
+    border-color: #0061df;
+    ::placeholder {
+      color: #000;
+    }
+  }
+`;
+
+Container.Button = styled.div`
+  width: 220px;
+  height: 55px;
+  background: var(--color-blue);
+  border-radius: 3px;
+  color: var(--color-white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 17px;
+  cursor: pointer;
+  :active {
+    transform: scale(0.97);
+  }
+`;
+
+const ChangePassword = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  padding: 30px;
+`;
+
+Container.Img = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-white);
+  flex: 1;
+  max-width: 330px;
+  height: fit-content;
+  margin-left: 30px;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  padding: 30px;
+  img {
+    min-width: 269px;
+    min-height: 320px;
+    border-radius: 3px;
+  }
+  h2 {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    color: #0d263b;
+    margin-bottom: 20px;
+  }
+  span {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    color: #696969;
+    text-align: center;
+    margin-top: 10px;
+  }
+`;
+
+Container.Upload = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #ffffff;
+  border: 1px solid #0061df;
+  box-sizing: border-box;
+  border-radius: 3px;
+  transform: uppercase;
+  cursor: pointer;
+  width: 100%;
+  height: 55px;
+  margin-top: 20px;
+  color: #0061df;
+  font-weight: 600;
+  line-height: 17px;
+`;
+
+export { Container, Body, Items, ChangePassword };
